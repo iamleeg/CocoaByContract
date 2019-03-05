3020ee1e9b20ec7427632346d94fa2dbd27b68eb
Graham Lee <leeg@labrary.online>
2019-03-05 08:54:21 +0000

    Initial Commit

---
+++ CocoaByContract/CocoaByContract.h
@@ -0,0 +1,13 @@
+//
+//  CocoaByContract.h
+//  CocoaByContract
+//
+//  Created by Graham Lee on 05/03/2019.
+//  Copyright © 2019 Labrary, Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CocoaByContract : NSObject
+
+@end
---
+++ CocoaByContract/CocoaByContract.m
@@ -0,0 +1,13 @@
+//
+//  CocoaByContract.m
+//  CocoaByContract
+//
+//  Created by Graham Lee on 05/03/2019.
+//  Copyright © 2019 Labrary, Ltd. All rights reserved.
+//
+
+#import "CocoaByContract.h"
+
+@implementation CocoaByContract
+
+@end
